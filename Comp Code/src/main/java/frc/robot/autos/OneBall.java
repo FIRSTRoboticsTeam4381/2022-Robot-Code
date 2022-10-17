4be5d75896f433edbc737f19e2c3effedf9f5635
Ryan <rzarnold05@outlook.com>
2022-10-16 21:47:26 -0400

    added one ball auto, sorry matt

---
+++ Comp Code/src/main/java/frc/robot/autos/OneBall.java
@@ -0,0 +1,75 @@
+package frc.robot.autos;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.IntakeIndex;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Swerve;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+
+public class OneBall extends SequentialCommandGroup {
+
+    private String trajectoryJSON1 = "paths/3BallLeg1.wpilib.json";
+    private Trajectory testTrajectory1 = new Trajectory();
+
+    public OneBall(Swerve s_Swerve, IntakeIndex intakeIndex, Shooter shooter) {
+        TrajectoryConfig config = new TrajectoryConfig(
+                Constants.AutoConstants.kMaxSpeedMetersPerSecond,
+                Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                        .setKinematics(Constants.Swerve.swerveKinematics);
+
+        try {
+            Path trajectoryPath1 = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON1);
+            testTrajectory1 = TrajectoryUtil.fromPathweaverJson(trajectoryPath1);
+        } catch (IOException ex) {
+            DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON1, ex.getStackTrace());
+        }
+
+        var thetaController = new ProfiledPIDController(
+                Constants.AutoConstants.kPThetaController, 0, 0, Constants.AutoConstants.kThetaControllerConstraints);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+        SwerveControllerCommand swerveControllerCommand1 = new SwerveControllerCommand(
+                testTrajectory1,
+                s_Swerve::getPose,
+                Constants.Swerve.swerveKinematics,
+                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+                new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+                thetaController,
+                s_Swerve::setModuleStates,
+                s_Swerve);
+        
+        addCommands(
+                new InstantCommand(() -> s_Swerve.resetOdometry(testTrajectory1.getInitialPose())),
+                new InstantCommand(() -> shooter.spinUP(Constants.shooterSpeedPercent)),
+                new WaitUntilCommand(() -> intakeIndex.startMatchReady()).andThen(new InstantCommand(() -> intakeIndex.resetState())),
+                new InstantCommand(() -> intakeIndex.fireBalls(true)),
+                new WaitUntilCommand(() -> !intakeIndex.getEye(2)), 
+                new InstantCommand(() -> intakeIndex.fireBalls(false)),
+                new InstantCommand(() -> shooter.spinUP(0)),
+                new InstantCommand(() -> intakeIndex.resetShot()),
+                new WaitCommand(10),
+                swerveControllerCommand1, 
+                new InstantCommand(() -> s_Swerve.drive(new Translation2d(0, 0), 0, true, false))
+                );
+    }
+}
